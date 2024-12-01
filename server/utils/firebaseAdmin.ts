9fe95c2d9b40613b15574b8aa0083500def6cbd2
unmilley <1337@milley.uno>
2024-12-01 16:01:31 +0500

    feat: `add getUser from cookie`

--- server/utils/firebaseAdmin.ts
+++ server/utils/firebaseAdmin.ts
@@ -1,12 +1,15 @@
 import type { ServiceAccount } from 'firebase-admin'
 import { cert, getApps, initializeApp } from 'firebase-admin/app'
+import { getAuth, type DecodedIdToken } from 'firebase-admin/auth'
 import type { EventHandlerRequest, H3Event } from 'h3'
+import type { DecodedIdTokenKeys } from '~/shared/types'
+import { REFRESH_TOKEN } from '~/src/runtimeConfig'
 
 export const defineFirebaseAdmin = (event: H3Event<EventHandlerRequest>) => {
   try {
     if (getApps().length === 0) {
       const config = useRuntimeConfig(event)
-      const firebaseConfig = config.fbAdmin as ServiceAccount
+      const firebaseConfig = config.firebaseServer as ServiceAccount
 
       initializeApp({
         credential: cert(firebaseConfig),
@@ -14,5 +17,21 @@ export const defineFirebaseAdmin = (event: H3Event<EventHandlerRequest>) => {
     }
   } catch (err) {
     return { err }
+  }
+}
+
+export const getUser = async (
+  event: H3Event<EventHandlerRequest>,
+  ...keys: DecodedIdTokenKeys[]
+): Promise<Partial<DecodedIdToken> | DecodedIdToken> => {
+  const cookie = getCookie(event, REFRESH_TOKEN)
+  if (!cookie) return {}
+  try {
+    const user = await getAuth().verifySessionCookie(cookie ?? '')
+    return keys.length
+      ? (Object.fromEntries(keys.map((val) => [val, user[val]])) as Partial<DecodedIdToken>)
+      : (user as DecodedIdToken)
+  } catch {
+    return {}
   }
 }
